23654ef0473f7aefe0aaf18f00045655767f7096
Agnieszka Wojdecka <77381@student.ue.poznan.pl>
2023-05-21 10:38:00 +0200

    New task

--- solo-work/solo_2.py
+++ solo-work/solo_2.py
@@ -4,3 +4,18 @@ def trojkat(bok_a, bok_b, bok_c, h_a):
     return pole, obwod
 
 print(f'Pole i obwód wynoszą: {trojkat(10, 15, 12, 0)}')
+
+def kolo(promien):
+    pole = 3.14 * (promien ** 2)
+    obwod = 2 * 3.14 * promien
+    return pole, obwod
+    
+print(f'Pole i obwód wynoszą: {kolo(10)}')
+
+def trapezprostokatny(podstawaA, podstawaB, bok, wysokosc):
+    pole = (podstawaA + podstawaB) * wysokosc / 2 
+    obwod = podstawaA + podstawaB + bok + wysokosc
+    return pole, obwod
+
+print(f'Pole i obwód wynoszą: {trapezprostokatny(10, 5, 7, 3)}')
+
